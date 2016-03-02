a727018440f2f25ebc8b541d4195501883ffe705
Róbert Fonó <fonorobert@gmail.com>
2016-03-03 00:25:34 +0100

    Build basic parser for responses through serial

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/cu.wchusbserial1420", {
-  baudrate: 9600
+var usbDevice = process.argv[2];
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+
+var serialPort = new SerialPort(usbDevice, {
+  baudrate: 9600,
+  parser: serialport.parsers.readline("\n")
 });
 
 var util = require('util');
@@ -24,11 +28,34 @@ var util = require('util');
     process.exit();
   }
 
+  function handleData(data) {
+    var sliced = data.replace(/(\r\n|\n|\r)/gm,"").split(':');
+    sliced.splice(0,1);
+    if (sliced.length > 0){
+      switch (sliced[0]) {
+        case "done":
+          console.log("Operation "+sliced[1]+" completed.");
+          break;
+        case "resp":
+          console.log("Value of "+sliced[1]+" is "+sliced[2]);
+          break;
+        case "serial":
+          if(sliced[1] == "begin") {
+            console.log("Serial connection open");
+          }
+        default:
+          console.log("can't handle this");
+          break;
+
+      }
+    }
+    // console.log(sliced);
+
+  }
+
 serialPort.on("open", function () {
   console.log('open');
-  serialPort.on('data', function(data) {
-    console.log('data received: ' + data);
-  });
+  serialPort.on('data', function(data) {handleData(data)});
   // serialPort.write("ledon\n", function(err, results) {
   //   console.log('err ' + err);
   //   console.log('results ' + results);
@@ -41,8 +68,10 @@ serialPort.on("open", function () {
       done();
     } else {
       serialPort.write(text.replace(/(\r\n|\n|\r)/gm,"") + "\r\n", function(err, results) {
-        console.log('err ' + err);
-        console.log('results ' + results);
+        if(err) {
+          console.log('err ' + err);
+        }
+        // console.log('results ' + results);
       });
     }
   });
